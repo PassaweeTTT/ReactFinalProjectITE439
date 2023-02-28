e4080c288fa3f78879ba1001d8d87a6dae903d1e
PassaweeTT <wa.passawee_st@tni.ac.th>
2023-02-28 16:14:42 +0700

    Bottom Tabs Done!!

--- dota2/App.js
+++ dota2/App.js
@@ -1,17 +1,44 @@
 import { View, Text, FlatList, ActivityIndicator } from "react-native";
 import React, { useEffect, useState } from "react";
+
+import { NavigationContainer} from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import RandomPage from "./component/RandomPage";
 import HeroPage from "./component/HeroPage";
 
-const App = () => {
+function HeroPageScreen() {
   return (
-    <View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <HeroPage/>
+    </View>
+  );
+}
+
+function RandomPageScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <RandomPage/>
     </View>
+  );
+}
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HeroPageScreen} />
+      <Tab.Screen name="Random" component={RandomPageScreen} />
+    </Tab.Navigator>
+  );
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 };
 
 export default App;
-
-
